94d79fb1d3775b8ed616e42d2f2bc2b64c8de7a9
MANUEL EDUARDO PALTA CARO <manu.eduardo@live.cl>
2021-10-10 23:39:28 -0300

    no se que wea es

--- Taller_DiseñoSistemas/pruebagit.Designer.cs
+++ Taller_DiseñoSistemas/pruebagit.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pruebagit));
             this.label1 = new System.Windows.Forms.Label();
             this.bunifuTileButton1 = new Bunifu.Framework.UI.BunifuTileButton();
+            this.bunifuHSlider1 = new Bunifu.UI.WinForms.BunifuHSlider();
             this.SuspendLayout();
             // 
             // label1
@@ -61,11 +62,59 @@
             this.bunifuTileButton1.Size = new System.Drawing.Size(128, 129);
             this.bunifuTileButton1.TabIndex = 1;
             // 
+            // bunifuHSlider1
+            // 
+            this.bunifuHSlider1.AllowCursorChanges = true;
+            this.bunifuHSlider1.AllowHomeEndKeysDetection = false;
+            this.bunifuHSlider1.AllowIncrementalClickMoves = true;
+            this.bunifuHSlider1.AllowMouseDownEffects = false;
+            this.bunifuHSlider1.AllowMouseHoverEffects = false;
+            this.bunifuHSlider1.AllowScrollingAnimations = true;
+            this.bunifuHSlider1.AllowScrollKeysDetection = true;
+            this.bunifuHSlider1.AllowScrollOptionsMenu = true;
+            this.bunifuHSlider1.AllowShrinkingOnFocusLost = false;
+            this.bunifuHSlider1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuHSlider1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuHSlider1.BackgroundImage")));
+            this.bunifuHSlider1.BindingContainer = null;
+            this.bunifuHSlider1.BorderRadius = 1;
+            this.bunifuHSlider1.BorderThickness = 1;
+            this.bunifuHSlider1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuHSlider1.DrawThickBorder = false;
+            this.bunifuHSlider1.DurationBeforeShrink = 2000;
+            this.bunifuHSlider1.ElapsedColor = System.Drawing.Color.DodgerBlue;
+            this.bunifuHSlider1.LargeChange = 10;
+            this.bunifuHSlider1.Location = new System.Drawing.Point(367, 274);
+            this.bunifuHSlider1.Maximum = 100;
+            this.bunifuHSlider1.Minimum = 0;
+            this.bunifuHSlider1.MinimumSize = new System.Drawing.Size(0, 31);
+            this.bunifuHSlider1.MinimumThumbLength = 18;
+            this.bunifuHSlider1.Name = "bunifuHSlider1";
+            this.bunifuHSlider1.OnDisable.ScrollBarBorderColor = System.Drawing.Color.Silver;
+            this.bunifuHSlider1.OnDisable.ScrollBarColor = System.Drawing.Color.Transparent;
+            this.bunifuHSlider1.OnDisable.ThumbColor = System.Drawing.Color.Silver;
+            this.bunifuHSlider1.ScrollBarBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            this.bunifuHSlider1.ScrollBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            this.bunifuHSlider1.ShrinkSizeLimit = 3;
+            this.bunifuHSlider1.Size = new System.Drawing.Size(200, 31);
+            this.bunifuHSlider1.SliderColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            this.bunifuHSlider1.SliderStyle = Bunifu.UI.WinForms.BunifuHSlider.SliderStyles.Thin;
+            this.bunifuHSlider1.SliderThumbStyle = Utilities.BunifuSlider.BunifuHScrollBar.SliderThumbStyles.Circular;
+            this.bunifuHSlider1.SmallChange = 1;
+            this.bunifuHSlider1.TabIndex = 2;
+            this.bunifuHSlider1.ThumbColor = System.Drawing.Color.DodgerBlue;
+            this.bunifuHSlider1.ThumbFillColor = System.Drawing.SystemColors.Control;
+            this.bunifuHSlider1.ThumbLength = 19;
+            this.bunifuHSlider1.ThumbMargin = 1;
+            this.bunifuHSlider1.ThumbSize = Bunifu.UI.WinForms.BunifuHSlider.ThumbSizes.Medium;
+            this.bunifuHSlider1.ThumbStyle = Bunifu.UI.WinForms.BunifuHSlider.ThumbStyles.Outline;
+            this.bunifuHSlider1.Value = 50;
+            // 
             // pruebagit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bunifuHSlider1);
             this.Controls.Add(this.bunifuTileButton1);
             this.Controls.Add(this.label1);
             this.Name = "pruebagit";
@@ -79,5 +128,6 @@
 
         private System.Windows.Forms.Label label1;
         private Bunifu.Framework.UI.BunifuTileButton bunifuTileButton1;
+        private Bunifu.UI.WinForms.BunifuHSlider bunifuHSlider1;
     }
 }
