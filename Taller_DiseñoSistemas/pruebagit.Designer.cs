f4e1387882862ef9f5e96d798b4a27bf1109917a
LuffyXD2016 Monkey <castillicatrifol@gmail.com>
2021-10-10 23:38:09 -0300

    Coloque un tile

--- Taller_DiseñoSistemas/pruebagit.Designer.cs
+++ Taller_DiseñoSistemas/pruebagit.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pruebagit));
             this.label1 = new System.Windows.Forms.Label();
+            this.bunifuTileButton1 = new Bunifu.Framework.UI.BunifuTileButton();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +42,31 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "prueba de sinc";
             // 
+            // bunifuTileButton1
+            // 
+            this.bunifuTileButton1.BackColor = System.Drawing.Color.SeaGreen;
+            this.bunifuTileButton1.color = System.Drawing.Color.SeaGreen;
+            this.bunifuTileButton1.colorActive = System.Drawing.Color.MediumSeaGreen;
+            this.bunifuTileButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuTileButton1.Font = new System.Drawing.Font("Century Gothic", 15.75F);
+            this.bunifuTileButton1.ForeColor = System.Drawing.Color.White;
+            this.bunifuTileButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuTileButton1.Image")));
+            this.bunifuTileButton1.ImagePosition = 20;
+            this.bunifuTileButton1.ImageZoom = 50;
+            this.bunifuTileButton1.LabelPosition = 41;
+            this.bunifuTileButton1.LabelText = "Tile 1";
+            this.bunifuTileButton1.Location = new System.Drawing.Point(153, 78);
+            this.bunifuTileButton1.Margin = new System.Windows.Forms.Padding(6);
+            this.bunifuTileButton1.Name = "bunifuTileButton1";
+            this.bunifuTileButton1.Size = new System.Drawing.Size(128, 129);
+            this.bunifuTileButton1.TabIndex = 1;
+            // 
             // pruebagit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bunifuTileButton1);
             this.Controls.Add(this.label1);
             this.Name = "pruebagit";
             this.Text = "pruebagit";
@@ -56,5 +78,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuTileButton bunifuTileButton1;
     }
 }
