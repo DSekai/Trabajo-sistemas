2476384a82d177afdbdfd3017b0f3181faad85bc
DSekai <92129971+DSekai@users.noreply.github.com>
2021-10-14 00:25:14 -0300

    Add files via upload

---
+++ Taller_DiseñoSistemas/Class/notas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_DiseñoSistemas.Class
+{
+    class notas
+    {
+        private string nombre;
+
+        public notas(string _nombre)
+        {
+            this.nombre = _nombre;
+        }
+        public DataTable ConsultarNotas()
+        {
+            DataTable notas = new DataTable();
+            string query = "call sp_consultaralumno_notas;";
+            Conexion consulta = new Conexion();
+            consulta.setQuery(query);
+            notas = consulta.ConsultaDatos();
+
+            return notas;
+        }
+
+        public DataSet ConsultarAsignatura()
+        {
+            DataSet notas = new DataSet();
+            string query = String.Format("call sp_consultaprosig('{0}');", this.nombre);
+            Conexion consulta = new Conexion();
+            consulta.setQuery(query);
+            notas = consulta.ConsultaDatos2();
+
+            return notas;
+        }
+    }
+}
