2b004c84339ee37071206b5c2f826bdbb8912701
DSekai <92129971+DSekai@users.noreply.github.com>
2021-10-14 21:51:58 -0300

    Add files via upload

---
+++ Taller_DiseñoSistemas/Class/anotaciones.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_DiseñoSistemas.Class
+{
+    class anotaciones
+    {
+        private String rut;
+   
+        private String fecha;
+        private String anotacion;
+        private String detalle;
+       
+
+        public anotaciones(String _rut, String _fecha, String _anotacion, String _detalle)
+        {
+            this.rut = _rut;
+            this.fecha = _fecha;
+            this.anotacion = _anotacion;
+            this.detalle = _detalle;
+           
+
+        }
+
+        public anotaciones(){}
+
+        public void ingresar_anotacion()
+        {
+            Conexion connect = new Conexion();
+
+            String query = String.Format("call sp_ingresar_anotaciones('{0}','{1}','{2}','{3}','{4}');",
+                this.rut,  this.fecha, this.anotacion, this.detalle);
+
+            connect.setQuery(query);
+
+            connect.EjecutarConsulta();
+        }
+
+        
+        public DataTable consultar_curso()
+        {
+            DataTable curso = new DataTable();
+
+            String query = "call sp_consultar_curso_anotacion;";
+
+            Conexion consulta = new Conexion();
+
+            consulta.setQuery(query);
+
+            curso = consulta.ConsultaDatos();
+
+            return curso;
+
+        }
+
+        public DataTable carga_dt()
+        {
+            DataTable curso = new DataTable();
+
+            String query = "call sp_consultar_alumno_anotacion;";
+
+            Conexion consulta = new Conexion();
+
+            consulta.setQuery(query);
+
+            curso = consulta.ConsultaDatos();
+
+            return curso;
+
+        }
+
+
+    }
+}
---
+++ Taller_DiseñoSistemas/Class/asistencia.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_DiseñoSistemas.Class
+{
+    class asistencia
+    {
+        private String rut;
+
+        private String fecha;
+       
+        private String asis;
+        private String asignatura;
+        public asistencia(String _rut, String _fecha, string _asistencia, string _asignatura)
+        {
+            this.rut = _rut;
+            this.fecha = _fecha;
+            this.asis = _asistencia;
+            this.asignatura = _asignatura;
+
+
+        }
+
+        public asistencia(string rut, string fecha, string asis)
+        {
+            this.rut = rut;
+            this.fecha = fecha;
+            this.asis = asis;
+        }
+
+        public void ingresar_asistencia()
+        {
+            Conexion connect = new Conexion();
+
+            String query = String.Format("call sp_ingresar_asistencia('{0}','{1}','{2}','{3}','{4}');",
+                this.rut, this.fecha, this.asis, this.asignatura);
+
+            connect.setQuery(query);
+
+            connect.EjecutarConsulta();
+        }
+
+        public DataTable consultar_curso()
+        {
+            DataTable curso = new DataTable();
+
+            String query = "call sp_consultar_curso_anotacion;";
+
+            Conexion consulta = new Conexion();
+
+            consulta.setQuery(query);
+
+            curso = consulta.ConsultaDatos();
+
+            return curso;
+
+        }
+    }
+}
